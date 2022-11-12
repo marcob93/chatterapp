49bf9c881d0a424082f6eedeaf566a736fec6f7e
livings08 <marcob93@hotmail.com>
2022-11-12 10:07:29 +0100

    Flutter & the Firebase SDK

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -12,6 +13,19 @@ class ChatScreen extends StatelessWidget {
           padding: const EdgeInsets.all(8),
           child: const Text('this works!'),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/VU2QA5FmJ0JP04oGc8Cw/messages')
+              .snapshots()
+              .listen((data) {
+            data.docs.forEach((element) {
+              print(element['text']);
+            });
+          });
+        },
       ),
     );
   }
