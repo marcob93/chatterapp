fc12f5efd6c313cd0bdd2a24601b391fc2d8ee8e
livings08 <marcob93@hotmail.com>
2022-11-12 11:07:25 +0100

    Adding Data

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -29,7 +29,11 @@ class ChatScreen extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/VU2QA5FmJ0JP04oGc8Cw/messages')
+              .add({'text': 'This was added by clicking the button!'});
+        },
       ),
     );
   }
